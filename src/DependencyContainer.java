b4276eb3740f0f5c47ea043a53b6e7c871e98065
linewelder <redalerm@gmail.com>
2023-09-03 18:46:12 +0200

    Add support for having several components of same type

--- src/DependencyContainer.java
+++ src/DependencyContainer.java
@@ -3,33 +3,49 @@ import java.util.*;
 
 public class DependencyContainer {
     /**
-     * Сопоставляет тип с компонентами этого типа.
-     * Если компонент в стадии создания, то типу сопоставлен null для обнаружения цикличных зависимостей.
+     * Звено в списке компонентов с одним типом, но разными квалификаторами.
      */
-    private static final Map<Class<?>, Object> components = new HashMap<>();
+    private static class NamedComponentNode {
+        final String qualifier;
+        /**
+         * Если null, - данный компонент в стадии создания.
+         */
+        Object component;
+        NamedComponentNode next = null;
 
-    public static <T> void addComponent(Class<?> klass, T component) {
+        NamedComponentNode(String qualifier, Object component) {
+            this.qualifier = qualifier;
+            this.component = component;
+        }
+    }
+
+    /**
+     * Сопоставляет типу компонент, или список компонентов, подставляемый при разрешении зависимостей.
+     */
+    private static final Map<Class<?>, NamedComponentNode> components = new HashMap<>();
+
+    public static <T> void addComponent(Class<?> klass, String qualifier, T component) {
         if (component == null) {
             throw new IllegalArgumentException("Component can't be null.");
         }
 
-        if (klass.getAnnotation(Component.class) == null) {
-            throw new IllegalArgumentException(klass + " does not have a Component attribute.");
+        if (setComponentInstance(klass, qualifier, component)) {
+            throw new IllegalArgumentException(
+                "The container already contains a " + klass +
+                " instance with qualifier '" + qualifier +"'.");
         }
+    }
 
-        if (!klass.isInstance(component)) {
-            throw new IllegalArgumentException("Component must be a " + klass + " instance.");
-        }
+    public static <T> void addComponent(Class<?> klass, T component) {
+        addComponent(klass, null, component);
+    }
 
-        if (components.containsKey(klass)) {
-            throw new IllegalArgumentException("The container already contains a " + klass + " instance.");
-        }
-
-        components.put(klass, component);
+    public static <T> void addComponent(String qualifier, T component) {
+        addComponent(component.getClass(), qualifier, component);
     }
 
     public static <T> void addComponent(T component) {
-        addComponent(component.getClass(), component);
+        addComponent(component.getClass(), null, component);
     }
 
     public static <T> T getComponent(Class<T> klass) {
@@ -38,19 +54,56 @@ public class DependencyContainer {
         }
 
         if (!components.containsKey(klass)) {
-            components.put(klass, null);
+            setComponentInstance(klass, null, null);
             final T instance = instantiate(klass);
-            components.put(klass, instance);
+            setComponentInstance(klass, null, instance);
             return instance;
         }
 
-        final Object object = components.get(klass);
-        if (object == null) {
+        final NamedComponentNode node = components.get(klass);
+        final Object component = node.component;
+        if (component == null) {
             throw new IllegalArgumentException(
                     "Cyclic dependency detected: " + klass + " instance is currently being created.");
         }
 
-        return klass.cast(object);
+        return klass.cast(component);
+    }
+
+    /**
+     * Добавляет компонент (может быть null) в контейнер заменяя существующий, если такой уже есть.
+     * @param klass Тип, который реализует добавляемый компонент.
+     * @param qualifier Опциональный квалификатор компонента.
+     * @param component Компонент или null.
+     * @param <T> Тип компонента.
+     * @return true, если такой компонент уже был в конейнере и был заменён, иначе - false.
+     */
+    private static <T> boolean setComponentInstance(Class<?> klass, String qualifier, T component) {
+        if (klass.getAnnotation(Component.class) == null) {
+            throw new IllegalArgumentException(klass + " does not have a Component attribute.");
+        }
+
+        if (component != null && !klass.isInstance(component)) {
+            throw new IllegalArgumentException("Component must be a " + klass + " instance.");
+        }
+
+        if (components.containsKey(klass)) {
+            NamedComponentNode node = components.get(klass);
+            do {
+                if (Objects.equals(qualifier, node.qualifier)) {
+                    node.component = component;
+                    return true;
+                }
+
+                node = node.next;
+            } while (node.next != null);
+
+            node.next = new NamedComponentNode(qualifier, component);
+            return false;
+        }
+
+        components.put(klass, new NamedComponentNode(qualifier, component));
+        return false;
     }
 
     private static <T> T instantiate(Class<T> klass) {
